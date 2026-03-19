723947a04c3dcd7f3b02960ff189333538f4d6e9
rahban <rahban.ghani2001@gmail.com>
2026-03-20 00:35:05 +0530

    vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
     devtools(),
+    nitro({
+      preset: process.env.VERCEL ? 'vercel' : 'node-server',
+      rollupConfig: {
+        external: [/^@sentry\//],
+      },
+    }),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
